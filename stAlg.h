dc0e65666ff6f3c320958986907b898e32620828
Choi1112 <103875494+Choi1112@users.noreply.github.com>
2022-04-17 21:25:51 +0900

    Update stAlg.h

--- stAlg.h
+++ stAlg.h
@@ -1,1 +1,14 @@
+#ifndef _STALG_
 
+#define _STALG_
+
+void exchangeSort(int* arr, int n);
+void mergeSort(int* arr, int start, int end);
+void merge(int start, int mid, int end, int* arr);
+void quickSort(int* arr, int l, int r);
+int partition(int* arr, int l, int h);
+void heapSort(int* arr, int n);
+void heap(int n, int i, int* arr);
+void radixSort(int* arr, int n);
+
+#endif
