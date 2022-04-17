820d9b115a3d82fdeac8851c7b0c2abdd9e77e74
Choi1112 <103875494+Choi1112@users.noreply.github.com>
2022-04-17 21:26:08 +0900

    Update tool.h

--- tool.h
+++ tool.h
@@ -1,1 +1,13 @@
+#include <time.h>
+#ifndef _TOOL_
 
+#define _TOOL_
+
+void swap(int* p, int* q);
+int* array(int n);
+void freeArray(int* arr);
+void randValue(int* arr, int n);
+void printArray(int* arr, int n);
+clock_t checkTime();
+
+#endif
