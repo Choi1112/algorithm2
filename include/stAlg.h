d1c8a791d1614673d9ef32375c331cf1aa8f3f9c
Choi1112 <103875494+Choi1112@users.noreply.github.com>
2022-04-18 20:03:10 +0900

    Update stAlg.h

--- include/stAlg.h
+++ include/stAlg.h
@@ -2,13 +2,13 @@
 
 #define _STALG_
 
-void exchangeSort(int* arr, int n);
-void mergeSort(int* arr, int start, int end);
-void merge(int start, int mid, int end, int* arr);
-void quickSort(int* arr, int l, int r);
-int partition(int* arr, int l, int h);
-void heapSort(int* arr, int n);
-void heap(int n, int i, int* arr);
-void radixSort(int* arr, int n);
+void exchangeSort(int* arr, int n);					// exchange sort 함수 
+void mergeSort(int* arr, int start, int end);		// merge sort 함수 
+void merge(int start, int mid, int end, int* arr);	// 합병 함수 
+void quickSort(int* arr, int l, int r);				// quick sort 함수
+int partition(int* arr, int l, int h);				// 분할 함수 
+void heapSort(int* arr, int n);						// heap sort 함수
+void heap(int n, int i, int* arr);					// MAX heap 
+void radixSort(int* arr, int n);					// radix sort 함수
 
 #endif
