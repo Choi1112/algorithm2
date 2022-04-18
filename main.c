76daba687f55a86d549ecd9659a3e6ad1c148efd
Choi1112 <103875494+Choi1112@users.noreply.github.com>
2022-04-18 19:58:03 +0900

    Update main.c

--- main.c
+++ main.c
@@ -7,64 +7,69 @@
 void main() {
 	int menu, n;
 	int* arr;
-	char* alg[5] = { "Exchange sort", "Merge sort", "Quick sort", "Heap sort", "Radix sort" };
-	clock_t start = 0, end = 0;
+	char* alg[5] = { "Exchange sort", "Merge sort", "Quick sort", "Heap sort", "Radix sort" }; // 정렬 알고리즘 메뉴 리스트 생성
+	clock_t start = 0, end = 0; // 수행 시간 할당받을 변수
 
-	printf("Size of data(100/500/1000/5000/10000) : ");
-	scanf("%d", &n);
-	arr = array(n); // 1차원 배열의 동적할당
-	randValue(arr, n); // 배열에 random 입력 데이터값 할당
+	while (1) {
+		printf("Sorting algoritm menu\n");
+		printf("1.Exchange sort\n2.Merge sort\n3.Quick sort\n4.Heap sort\n5.Radix sor\n6.Exit\n\n");
+		printf("Selected sorting algoritm menu number : ");	// 정렬 알고리즘 선택
+		scanf("%d", &menu);
+		if (menu == 6) {	// EXIT 메뉴 선택시 Program End
+			printf("Program End\n");
+			break;
+		}
+		printf("Size of data(100/500/1000/5000/10000) : ");	// 데이터 크기 입력
+		scanf("%d", &n);
 
-	printf("Sorting algoritm menu\n\n");
-	printf("1.Exchange sort\n2.Merge sort\n3.Quick sort\n4.Heap sort\n5.Radix sort\n\n");
-	printf("Selected sorting algoritm menu number : ");
-	scanf("%d", &menu);
-	printf("\n\n");
+		printf("\n%s", alg[menu-1]);
+		printf("Result\n");
 
-	printf("Original Array\n");
-	printArray(arr, n);
-	printf("\n\n");
+		for (int i = 0;i < 5;i++) {
+			arr = array(n); // 1차원 배열의 동적할당
+			randValue(arr, n); // 배열에 random 입력 데이터값 할당
 
-	printf("Result\n");
-	if (menu == 1) {
-		start = checkTime();
-		exchangeSort(arr, n);
-		printArray(arr, n);
-		end = checkTime();
-		printf("\n%s ", alg[0]);
-	}
-	else if (menu == 2) {
-		start = checkTime();
-		mergeSort(arr, 0, n - 1);
-		printArray(arr, n);
-		end = checkTime();
-		printf("\n%s ", alg[1]);
-	}
-	else if (menu == 3) {
-		start = checkTime();
-		quickSort(arr, 0, n - 1);
-		printArray(arr, n);
-		end = checkTime();
-		printf("\n%s ", alg[2]);
-	}
-	else if (menu == 4) {
-		start = checkTime();
-		heapSort(arr, n);
-		printArray(arr, n);
-		end = checkTime();
-		printf("\n%s ", alg[3]);
-	}
-	else if (menu == 5) {
-		start = checkTime();
-		radixSort(arr, n);
-		printArray(arr, n);
-		end = checkTime();
-		printf("\n%s ", alg[4]);
-	}
-	else {
-		printf("It doesn't support the sorting algorithm selected by user\n");
-	}
+			//printf("Original Array\n");
+			//printArray(arr, n);
+			printf("\n");
 
-	printf("execution time : %.3f\n", (float)(end - start) / CLOCKS_PER_SEC);
-	freeArray(arr);
+			if (menu == 1) {	// 교환 정렬 수행
+				start = checkTime();
+				exchangeSort(arr, n);
+				//printArray(arr, n);
+				end = checkTime();
+			}
+			else if (menu == 2) {	// 합병 정렬 수행
+				start = checkTime();
+				mergeSort(arr, 0, n - 1);
+				//printArray(arr, n);
+				end = checkTime();
+			}
+			else if (menu == 3) {	// 퀵 정렬 수행
+				start = checkTime();
+				quickSort(arr, 0, n - 1);
+				//printArray(arr, n);
+				end = checkTime();
+			}
+			else if (menu == 4) {	// 힙 정렬 수행
+				start = checkTime();
+				heapSort(arr, n);
+				//printArray(arr, n);
+				end = checkTime();
+			}
+			else if (menu == 5) {	// 기수 정렬 수행
+				start = checkTime();
+				radixSort(arr, n);
+				//printArray(arr, n);
+				end = checkTime();
+			}
+			else {	// 메뉴를 잘못입력한 경우(예외 처리)
+				printf("It doesn't support the sorting algorithm selected by user\n");
+			}
+
+			printf("%d. execution time : %.3f", i+1, (float)(end - start) / CLOCKS_PER_SEC);	// 알고리즘 수행시간 출력
+			freeArray(arr);	// 동적 할당 받은 배열 반환
+		}
+		printf("\n----------------------------------------------------------------------\n");
+	}
 }
