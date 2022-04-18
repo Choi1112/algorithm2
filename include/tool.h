4563e68a66cb2e5cd57f441318e301dbe6454cbf
Choi1112 <103875494+Choi1112@users.noreply.github.com>
2022-04-18 20:02:40 +0900

    Update tool.h

--- include/tool.h
+++ include/tool.h
@@ -3,11 +3,11 @@
 
 #define _TOOL_
 
-void swap(int* p, int* q);    // swap연산 수행
-int* array(int n);    // 1차원 배열 동적할당
-void freeArray(int* arr);   // 동적할당 메모리 반환
-void randValue(int* arr, int n);    // 랜덤값 생성, 배열에 할당
-void printArray(int* arr, int n);   // 배열 출력 함수
-clock_t checkTime();    // 프로세서 및 cpu 시간 측정
+void swap(int* p, int* q);				// swap연산 수행
+int* array(int n);						// 1차원 배열 동적할당
+void freeArray(int* arr);				// 동적할당 메모리 반환
+void randValue(int* arr, int n);		// 랜덤값 생성, 배열에 할당
+void printArray(int* arr, int n);		// 배열 출력 함수
+clock_t checkTime();					// 프로세서 및 cpu 시간 측정
 
 #endif
