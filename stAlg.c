9934441d83c59d636c5e1a20f05639a804d0209f
Choi1112 <103875494+Choi1112@users.noreply.github.com>
2022-04-18 19:58:59 +0900

    Update stAlg.c

--- stAlg.c
+++ stAlg.c
@@ -2,12 +2,13 @@
 #include "tool.h"
 #include "stAlg.h"
 
+// 교환 정렬 함수 
 void exchangeSort(int* arr, int n) {
 	int i, j;
 	int temp;
 	for (i = 0;i < n;i++) {
 		for (j = i + 1;j < n;j++) {
-			if (arr[i] > arr[j]) {
+			if (arr[i] > arr[j]) {	// 데이터 비교, swap연산 수행 
 				temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;
@@ -16,15 +17,17 @@ void exchangeSort(int* arr, int n) {
 	}
 }
 
-void mergeSort(int* arr, int start, int end) {
+// 합병 정렬 : divide and conquer
+void mergeSort(int* arr, int start, int end) {	// 배열의 시작 인덱스, 끝 인덱스 
 	if (start < end) {
 		int mid = (start + end) / 2;
-		mergeSort(arr, start, mid);
+		mergeSort(arr, start, mid);		// 하위 목록으로 분할
 		mergeSort(arr, mid + 1, end);
-		merge(start, mid, end, arr);
+		merge(start, mid, end, arr);	// 병합
 	}
 }
 
+// 병합 함수
 void merge(int start, int mid, int end, int* arr) {
 	int* result;
 	int i = start;
@@ -32,7 +35,7 @@ void merge(int start, int mid, int end, int* arr) {
 	int k = 0;
 	int q;
 
-	result = array(end - start + 1);
+	result = array(end - start + 1);	//추가적인 배열 공간 사용
 
 	while (i <= mid && j <= end) {
 		if (arr[i] <= arr[j]) {
@@ -60,6 +63,7 @@ void merge(int start, int mid, int end, int* arr) {
 		j++;
 	}
 
+	// 배열에 정렬된 값 할당
 	for (i = start; i <= end; i++) {
 		q = i - start;
 		arr[i] = result[q];
@@ -68,56 +72,61 @@ void merge(int start, int mid, int end, int* arr) {
 	freeArray(result);
 }
 
+// 퀵 정렬 구현 함수 
 void quickSort(int* arr, int l, int r) {
 	if (l < r) {
-		int j = partition(arr, l, r);
-		quickSort(arr, l, j - 1);
-		quickSort(arr, j + 1, r);
+		int j = partition(arr, l, r); // pivot의 인덱스, 배열에 정렬된 pivot 위치   
+		quickSort(arr, l, j - 1);	// pivot 인덱스 하위 목록
+		quickSort(arr, j + 1, r);	// pivot 인덱스 상위 목록
 	}
 }
 
+// pivot 설정, 배열에 pivot 요소를 올바른 위치로 옮김 
 int partition(int* arr, int l, int h){
-	int pivot = arr[h];
+	int pivot = arr[h];	// arr 배열의 마지막 요소 pivot 설정
 	int i = l - 1;
 
 	for (int j = l; j <= h - 1;j++) {
-		if (arr[j] <= pivot) {
+		if (arr[j] <= pivot) {	// 배열의 arr[j] 값 pivot보다 작거나 같으면 arr[i], arr[j] 두 값 swap연산 
 			i++;
 			swap(&arr[i], &arr[j]);
 		}
 	}
 
-	swap(&arr[i + 1], &arr[h]);
+	swap(&arr[i + 1], &arr[h]); // 마지막으로 올바른 위치에 pivot 오도록 swap
 	return (i + 1);
 }
 
+// 힙 정렬 구현 함수
 void heapSort(int* arr, int n) {
 	for (int i = (n / 2) - 1; i >= 0; i--) {
-		heap(n, i, arr);
+		heap(n, i, arr);	// 초기의 최대 힙 생성 (최대 힙 - 상위 노드가 두 하위 노드보다 큰 이진트리)	
 	}
-	for (int i = n - 1; i >= 0;i--) {
+	for (int i = n - 1; i >= 0;i--) {	// 가장 큰 요소(root) 바꾸고, heap() - 최대 힙 생성 함수 수행 
 		swap(&arr[0], &arr[i]);
 		heap(i, 0, arr); 
 	}
 }
 
+// 최대 힙 생성 함수 
 void heap(int n, int i, int* arr) {
-	int left = 2 * i + 1;
-	int right = 2 * i + 2;
+	int left = 2 * i + 1;	// 왼쪽 child에 해당하는 배열 인덱스
+	int right = 2 * i + 2;	// 오른쪽 child에 해당하는 배열 인덱스
 	int largest = i;
 
-	if (left<n && arr[left] >arr[largest]) {
+	if (left<n && arr[left] >arr[largest]) {	// 왼쪽 child 존재여부 && root 보다 큰지 확인 
 		largest = left;
 	}
-	if (right<n && arr[right]>arr[largest]) {
+	if (right<n && arr[right]>arr[largest]) {	// 오른쪽 child 존재여부 && root 보다 큰지 확인
 		largest = right;
 	}
-	if (largest != i) {
-		swap(&arr[i], &arr[largest]);
-		heap(n, largest, arr);
+	if (largest != i) {	// root가 가장 크지 않는 경우
+		swap(&arr[i], &arr[largest]);	// swap 연산 수행하여 루트를 가장 크게 만든다.
+		heap(n, largest, arr);			// 최대 힙 구현까지 반복 수행
 	}
 }
 
+// 기수 정렬 구현 함수 
 void radixSort(int* arr, int n) {
 	int temp[10000];
 	int max = 0;
@@ -125,26 +134,26 @@ void radixSort(int* arr, int n) {
 
 	for (int i = 0;i < n;i++) {
 		if (arr[i] > max) {
-			max = arr[i];
+			max = arr[i];	// 최대 자릿수를 가진 배열의 최댓값 
 		}
 	}
 
-	while (max / digit > 0) {
+	while (max / digit > 0) {	// max값이 자릿수보다 클 때 반복, 작으면 종료 
 		int bucket[10] = { 0 }; // 0 ~ 9
 		for (int i = 0; i < n;i++) {
-			bucket[(arr[i] / digit) % 10]++;
+			bucket[(arr[i] / digit) % 10]++;	// 자릿수 배열 bucket 처리 함수 (나머지 이용)
 		}
 		for (int i = 1; i < 10;i++) {
-			bucket[i] += bucket[i - 1];
+			bucket[i] += bucket[i - 1];			// 누적 자릿수 배열 처리
 		}
 
 		for (int i = n - 1;i >= 0;i--) {
-			temp[--bucket[arr[i] / digit % 10]] = arr[i];
+			temp[--bucket[arr[i] / digit % 10]] = arr[i];	// temp 배열에 값들이 정렬되어 할당된다. 
 		}
 
 		for (int i = 0; i < n;i++) {
-			arr[i] = temp[i];
+			arr[i] = temp[i];	// 정렬된 배열로 기존 배열 갱신 
 		}
-		digit *= 10;
+		digit *= 10;	// 자릿수 증가
 	}
 }
