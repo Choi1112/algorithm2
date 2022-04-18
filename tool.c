e76a5ddbf8fdec23ed3ff6e6803b661dba272344
Choi1112 <103875494+Choi1112@users.noreply.github.com>
2022-04-18 19:59:29 +0900

    Update tool.c

--- tool.c
+++ tool.c
@@ -3,23 +3,26 @@
 #include <time.h>
 #include "tool.h"
 
-void swap(int* p, int* q)
-{
+// 두 변수에 들어 있는 값 서로 맞바꾸는 연산
+void swap(int* p, int* q) {
     int temp = *p;
     *p = *q;
     *q = temp;
 }
 
+// 데이터 크기만큼의 1차원 배열 동적 할당
 int* array(int n) {
     int* arr;
     arr = (int*)malloc(sizeof(int) * n);
     return arr;
 }
 
+// 동적 메모리 반환
 void freeArray(int* arr) {
     free(arr);
 }
 
+// 배열에 0 ~ 9999 사이 랜덤값을 배열에 할당
 void randValue(int* arr, int n) {
     int i;
     srand((unsigned)time(NULL));
@@ -28,6 +31,7 @@ void randValue(int* arr, int n) {
     }
 }
 
+// 배열을 출력하는 함수
 void printArray(int* arr, int n) {
     int i;
     if (n <= 20) {
@@ -40,6 +44,7 @@ void printArray(int* arr, int n) {
     }
 }
 
+// 프로세스, CPU 실행시간 측정 코드
 clock_t checkTime() {
     clock_t time = clock();
     return time;
